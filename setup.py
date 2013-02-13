e09fe44a19b9fd8ca32b70cdbf1cc504bb95a85c
Krisztian Fekete <fkr972@gmail.com>
2013-02-14 00:58:22 +0100

    initial packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf8
+
+# distutils is nice and recommended, but pip will not install requires=
+# requirements :(, so going with setuptools - as everyone else!?
+# from distutils.core import setup
+from setuptools import setup
+
+setup(name='externals',
+      version='0.1dev',
+      description=u'A light abstraction of hierarchically named resources,'
+                  u' potentially external to the current process',
+      author=u'Krisztián Fekete',
+#       author_email='fkr972',
+#       url='http://maybe.later',
+      packages=['externals'],
+      install_requires=['tempdir >=0.4, <1.0'],
+      test_requires=['nose >=0.13'],
+      provides=['externals (0.1)']
+     )
