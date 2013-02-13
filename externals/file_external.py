39a0c6cabbec0323c1b47a051986b95917942403
Krisztian Fekete <fkr972@gmail.com>
2013-02-13 23:42:13 +0100

    file externals

---
+++ externals/file_external.py
@@ -0,0 +1,75 @@
+from external import External
+from external import NoParentError
+import os
+import shutil
+
+
+class FileExternal(External):
+
+    def __init__(self, path):
+        self._path = os.path.realpath(path)
+
+    @property
+    def name(self):
+        parent, tail = os.path.split(self._path)
+        return tail
+
+    def parent(self):
+        new_path, tail = os.path.split(self._path)
+        if not tail:
+            raise NoParentError
+        return FileExternal(new_path)
+
+    def child(self, name):
+        return FileExternal(os.path.join(self._path, name))
+
+    def __add__(self, sub_path):
+        ''' Syntactic sugar for child(u'name1').child(u'name2')...
+
+        x + u'name'
+        x + u'name1/name2/name3'
+        '''
+        return FileExternal(
+            os.path.join(
+                self._path,
+                sub_path.strip(os.path.sep)))
+
+    def exists(self):
+        return os.path.exists(self._path)
+
+    def is_file(self):
+        return os.path.isfile(self._path)
+
+    def content(self):
+        with self.readable_stream() as f:
+            return f.read()
+
+    def set_content(self, content):
+        with self.writable_stream() as f:
+            f.write(content)
+
+    def readable_stream(self):
+        return open(self._path, u'rb')
+
+    def writable_stream(self):
+        return open(self._path, u'wb')
+
+    def is_dir(self):
+        return os.path.isdir(self._path)
+
+    def __iter__(self):
+        for name in os.listdir(self._path):
+            yield self.child(name)
+
+    def children(self):
+        return list(self)
+
+    def create(self, content):
+        parent, tail = os.path.split(self._path)
+        if not os.path.exists(parent):
+            os.makedirs(parent)
+
+        self.set_content(content)
+
+    def remove(self):
+        shutil.rmtree(self._path)
---
+++ externals/test_file_external.py
@@ -0,0 +1,175 @@
+# coding: utf8
+import unittest
+import os
+from tempdir import TempDir
+import file_external as m
+
+
+class TestFileExternal(unittest.TestCase):
+
+    def test_parent_of_root_exception(self):
+        x = m.FileExternal(u'/')
+        with self.assertRaises(m.NoParentError):
+            x.parent()
+
+    def test_child_of_root_has_a_parent(self):
+        x = m.FileExternal(u'/')
+        child = x.child(u'stem')
+        child.parent()
+
+    def test_this_file_exists(self):
+        x = m.FileExternal(__file__)
+        self.assertTrue(x.exists())
+
+    def test_nonexistent_sibling_does_not_exists(self):
+        x = m.FileExternal(__file__).parent().child('nonexistent')
+        self.assertFalse(x.exists())
+
+    def test_this_file_is_a_file(self):
+        x = m.FileExternal(__file__)
+        self.assertTrue(x.is_file())
+
+    def test_this_file_is_not_a_directory(self):
+        x = m.FileExternal(__file__)
+        self.assertFalse(x.is_dir())
+
+    def test_root_is_not_a_file(self):
+        x = m.FileExternal(u'/')
+        self.assertFalse(x.is_file())
+
+    def test_root_is_a_directory(self):
+        x = m.FileExternal(u'/')
+        self.assertTrue(x.is_dir())
+
+    def test_name_is_last_segment_of_path(self):
+        x = m.FileExternal(u'/a/last')
+        self.assertEqual(u'last', x.name)
+
+    def test_content_returns_file_content(self):
+        with TempDir() as d:
+            filename = os.path.join(d.name, u'test-file1')
+            with open(filename, u'wb') as f:
+                f.write('something\nand more')
+
+            x = m.FileExternal(filename)
+
+            self.assertEqual('something\nand more', x.content())
+
+    def test_set_content_stores_data(self):
+        with TempDir() as d:
+            filename = os.path.join(d.name, u'test-file2')
+
+            x_store = m.FileExternal(filename)
+            x_store.set_content('something2\nand more')
+
+            x_read = m.FileExternal(filename)
+            self.assertEqual('something2\nand more', x_read.content())
+
+    def test_readable_stream_returns_an_open_file(self):
+        with TempDir() as d:
+            filename = os.path.join(d.name, u'test-file3')
+
+            x_store = m.FileExternal(filename)
+            x_store.set_content('something3')
+
+            x_read = m.FileExternal(filename)
+            with x_read.readable_stream() as stream:
+                self.assertEqual('s', stream.read(1))
+                self.assertEqual('o', stream.read(1))
+                self.assertEqual('mething3', stream.read())
+
+    def test_writable_stream_returns_an_open_file(self):
+        with TempDir() as d:
+            x_tempdir = m.FileExternal(d.name)
+
+            x_store = x_tempdir.child(u'test-file')
+            with x_store.writable_stream() as stream:
+                stream.write('s')
+                stream.write('o')
+                stream.write('mething4')
+
+            x_read = x_tempdir.child(u'test-file')
+            self.assertEqual('something4', x_read.content())
+
+    def test_children_returns_list_of_externals_for_children(self):
+        with TempDir() as d:
+            x_tempdir = m.FileExternal(d.name)
+            x_tempdir.child(u'a').create('a content')
+            x_tempdir.child(u'b').create('b content')
+            os.mkdir(os.path.join(d.name, 'c'))
+
+            x_test = m.FileExternal(d.name)
+            def name(x):
+                return x.name
+            children = sorted(x_test.children(), key=name)
+
+            self.assertEqual(3, len(children))
+            self.assertEqual([u'a', u'b', u'c'], map(name, children))
+
+    def test_external_is_an_iterable_of_its_children(self):
+        with TempDir() as d:
+            x_tempdir = m.FileExternal(d.name)
+            x_tempdir.child(u'a').create('a content')
+            x_tempdir.child(u'b').create('b content')
+            os.mkdir(os.path.join(d.name, 'c'))
+
+            x_test = m.FileExternal(d.name)
+            children_list = []
+            # iterate over the external
+            for child in x_test:
+                children_list.append(child)
+
+            def name(x):
+                return x.name
+            children = sorted(x_test.children(), key=name)
+
+            self.assertEqual(3, len(children))
+            self.assertEqual([u'a', u'b', u'c'], map(name, children))
+
+    def test_create_creates_missing_directories_and_a_file(self):
+        with TempDir() as d:
+            x_tempdir = m.FileExternal(d.name)
+            x_a = x_tempdir.child(u'a')
+            x_ab = x_a.child(u'b')
+            x_file = x_ab.child(u'c')
+
+            x_file.create('content')
+
+            with open(os.path.join(d.name, u'a/b/c')) as f:
+                self.assertEqual('content', f.read())
+
+    def test_directory_with_subdir_is_removed(self):
+        with TempDir() as d:
+            x_tempdir = m.FileExternal(d.name)
+            x_a = x_tempdir.child(u'a')
+            x_ab = x_a.child(u'b')
+            x_file = x_ab.child(u'c')
+
+            x_file.create('content')
+
+            x_a.remove()
+
+            self.assertFalse(x_ab.exists())
+
+    def test_adding_a_string_to_an_external_is_the_same_as_asking_for_a_child(self):
+        with TempDir() as d:
+            x_tempdir = m.FileExternal(d.name)
+            x_tempdir.child(u'a').create('child')
+
+            self.assertEqual('child', (x_tempdir + u'a').content())
+
+    def test_add_a_path(self):
+        with TempDir() as d:
+            x_tempdir = m.FileExternal(d.name)
+            # file dir-a/dir-b/dir-c
+            x_tempdir.child(u'dir-a').child(u'dir-b').child('dir-c').create('child')
+
+            self.assertEqual('child', (x_tempdir + u'dir-a/dir-b/dir-c').content())
+
+    def test_add_a_path_wrapped_in_slashes(self):
+        with TempDir() as d:
+            x_tempdir = m.FileExternal(d.name)
+            # file dir-a/dir-b/dir-c
+            x_tempdir.child(u'dir-a').child(u'dir-b').child('dir-c').create('child')
+
+            self.assertEqual('child', (x_tempdir + u'/dir-a/dir-b/dir-c/').content())
