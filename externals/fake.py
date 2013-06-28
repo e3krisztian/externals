afadc0578a3df5cdf77652b97cec92058dc44dbc
Krisztian Fekete <fkr972@gmail.com>
2013-06-28 17:14:52 +0200

    Fake: in-memory external

---
+++ externals/fake.py
@@ -0,0 +1,134 @@
+from externals.external import External, NoParentError
+from StringIO import StringIO
+import contextlib
+
+
+PATH_SEPARATOR = '/'
+
+# FS nodes are dictionaries, with known keys (all of them are optional):
+# additional metadata can be supported by adding new keys
+
+KEY_CHILDREN = 'children'  # directory behavior
+KEY_CONTENT = 'content'  # file behavior
+
+
+class FS(object):
+
+    def __init__(self):
+        self.root = {}
+
+    def __getitem__(self, path):
+        node = self.root
+        for name in path:
+            if KEY_CHILDREN not in node:
+                raise KeyError(path)
+            subdirs = node[KEY_CHILDREN]
+            node = subdirs[name]
+        return node
+
+    def ensure(self, path):
+        node = self.root
+        for name in path:
+            if KEY_CHILDREN not in node:
+                node[KEY_CHILDREN] = {}
+            subdirs = node[KEY_CHILDREN]
+            if name not in subdirs:
+                subdirs[name] = {}
+            node = subdirs[name]
+
+
+class Fake(External):
+
+    '''I am not an external, but pretend to be: hold data in memory.'''
+
+    def __init__(self, fs=None, path=None):
+        self._fs = fs or FS()
+        self._path = path or ()
+        assert isinstance(self._path, tuple)
+
+    @property
+    def _node(self):
+        return self._fs[self._path]
+
+    def _new(self, path):
+        return self.__class__(self._fs, path)
+
+    def parent(self):
+        if not self._path:
+            raise NoParentError
+        return self._new(self._path[:-1])
+
+    def child(self, name):
+        assert PATH_SEPARATOR not in name
+        assert name
+        return self._new(self._path + (name,))
+
+    def exists(self):
+        try:
+            self._node
+            return True
+        except KeyError:
+            return False
+
+    @property
+    def name(self):
+        return self._path[-1] if self._path else ''
+
+    def __div__(self, sub_path):
+        ''' Syntactic sugar for child(u'name1').child(u'name2')...
+
+        x / u'name'
+        x / u'name1/name2/name3'
+        '''
+        return self._new(self._path + tuple(sub_path.split(PATH_SEPARATOR)))
+
+    def is_file(self):
+        try:
+            return KEY_CONTENT in self._node
+        except KeyError:
+            return False
+
+    def content(self):
+        return self._node[KEY_CONTENT]
+
+    def set_content(self, content):
+        '''Create file with content and missing directories up to the file'''
+        self._fs.ensure(self._path)
+        self._node[KEY_CONTENT] = content
+
+    def readable_stream(self):
+        return contextlib.closing(StringIO(self.content()))
+
+    def writable_stream(self):
+        return contextlib.closing(WritableStream(self))
+
+    def is_dir(self):
+        try:
+            return KEY_CHILDREN in self._node
+        except KeyError:
+            return False
+
+    def __iter__(self):
+        ''' Iterator over children '''
+        subdir = self._node[KEY_CHILDREN]
+        return (
+            self / name
+            for name in subdir)
+
+    def remove(self):
+        try:
+            self._node.clear()
+        except KeyError:
+            pass
+
+
+class WritableStream(StringIO):
+
+    def __init__(self, external):
+        StringIO.__init__(self)
+        self.__external = external
+
+    def close(self):
+        content = self.getvalue()
+        StringIO.close(self)
+        self.__external.set_content(content)
---
+++ externals/test/test_fake.py
@@ -0,0 +1,171 @@
+# coding: utf8
+import unittest
+import externals.fake as m
+
+ROOT_PATH = ()
+
+
+class Test_FS(unittest.TestCase):
+
+    def test_access_root(self):
+        fs = m.FS()
+        self.assertEqual(fs.root, fs[ROOT_PATH])
+
+    def test_access_non_existing(self):
+        with self.assertRaises(KeyError):
+            m.FS()[('nonexistent child')]
+
+    def test_access_non_existing2(self):
+        fs = m.FS()
+        fs.ensure(['a'])
+        with self.assertRaises(KeyError):
+            fs[('nonexistent child')]
+
+    def assertPathExists(self, fs, path):
+        name = path[-1]
+        parent_path = fs[path[:-1]]
+        self.assertIn(name, parent_path[m.KEY_CHILDREN])
+
+    def test_create_child_of_root(self):
+        fs = m.FS()
+        path = ['a']
+        fs.ensure(path)
+        self.assertPathExists(fs, path)
+
+    def test_set_far_from_root(self):
+        fs = m.FS()
+        path = ['a', 'b', 'c']
+        fs.ensure(path)
+        self.assertPathExists(fs, path)
+
+    def test_two_children(self):
+        fs = m.FS()
+
+        path1 = ['a', 'b', 'c1']
+        fs.ensure(path1)
+        self.assertPathExists(fs, path1)
+
+        path2 = ['a', 'b', 'c2']
+        fs.ensure(path2)
+        self.assertPathExists(fs, path1)
+        self.assertPathExists(fs, path2)
+
+
+class Test_Fake(unittest.TestCase):
+
+    def test_child_of_root_has_a_parent(self):
+        x = m.Fake()
+        child = x.child('a')
+        child.parent()
+
+    def test_root_has_no_parent(self):
+        x = m.Fake()
+        with self.assertRaises(m.NoParentError):
+            x.parent()
+
+    def test_name(self):
+        self.assertEqual('a name', (m.Fake() / 'a name').name)
+
+    def test_nonexistent_child_does_not_exists(self):
+        x = m.Fake().child('nonexistent')
+        self.assertFalse(x.exists())
+
+    def test_root_exists(self):
+        self.assertTrue(m.Fake().exists())
+
+    def test_existing_child_exists(self):
+        x = m.Fake() / 'b'
+        x.set_content('b content')
+        self.assertTrue(x.exists())
+
+    def test_content(self):
+        x = m.Fake() / 'b'
+        x.set_content('b content')
+        self.assertEqual('b content', x.content())
+
+    def test_nodes_on_the_path_to_existing_content_exist(self):
+        a = m.Fake() / 'a'
+        b = a / 'b'
+        b.set_content('a/b content')
+        self.assertTrue(a.exists())
+
+    def test_node_with_content_is_file(self):
+        x = m.Fake() / 'b'
+        x.set_content('b content')
+        self.assertTrue(x.is_file())
+
+    def test_nonexisting_node_is_not_file(self):
+        x = m.Fake() / 'b'
+        self.assertFalse(x.is_file())
+
+    def test_parent_of_file_is_dir(self):
+        x = m.Fake() / 'a' / 'b'
+        x.set_content(u'a/b content')
+        self.assertTrue(x.parent().is_dir())
+
+    def test_new_instance_is_neither_a_file_nor_a_dir(self):
+        x = m.Fake()
+        self.assertFalse(x.is_file())
+        self.assertFalse(x.is_dir())
+
+    def test_readable_stream(self):
+        x = m.Fake()
+        x.set_content(u'I am root')
+        with x.readable_stream() as f:
+            self.assertEqual(u'I am root', f.read())
+
+    def test_writable_stream(self):
+        x = m.Fake() / 'file123'
+        with x.writable_stream() as f:
+            f.write(u'FILE')
+            f.write(u'1')
+            f.write(u'23')
+        self.assertEqual(u'FILE123', x.content())
+
+    def test_children(self):
+        x = m.Fake()
+        (x / 'a' / 'b').set_content('content of a/b')
+        (x / 'x').set_content('content of x')
+
+        def name(x):
+            return x.name
+        a, x = sorted(x.children(), key=name)
+
+        self.assertEqual('a', a.name)
+        self.assertEqual('content of x', x.content())
+
+    def test_remove_root_removes_all_contents(self):
+        root = m.Fake()
+        root.set_content('root')
+        ab = root / 'a' / 'b'
+        ab.set_content('content of a/b')
+
+        root.remove()
+
+        with self.assertRaises(KeyError):
+            root.content()
+        self.assertFalse(ab.exists())
+
+    def test_remove_a_non_root(self):
+        root = m.Fake()
+        root.set_content('root')
+        x = root / '1'
+        ab = x / 'a' / 'b'
+        ab.set_content('content of a/b')
+
+        x.remove()
+
+        self.assertFalse(ab.exists())
+        self.assertTrue('root', root.content())
+        with self.assertRaises(KeyError):
+            ab.content()
+
+    def test_nonexistent_path_is_not_a_directory(self):
+        self.assertFalse((m.Fake() / 'a').is_dir())
+
+    def test_remove_nonexistent_path(self):
+        root = m.Fake()
+        (root / 'x').set_content('expect to remain')
+        (root / 'a').remove()
+
+        self.assertEqual('expect to remain', (root / 'x').content())
